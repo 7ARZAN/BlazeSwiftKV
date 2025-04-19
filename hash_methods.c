b31083cd9c1a1f47d950b30b40ed4e5caf0b22d2
elhousseine lakhfif <akaouam1@gmail.com>
2025-04-19 19:52:12 +0100

    Norm files Rush_ [tarzan]

--- hash_methods.c
+++ hash_methods.c
@@ -5,22 +5,23 @@
 */
 uint64_t	wyhash(const char *str)
 {
-    uint64_t	seed;
-    const uint8_t	*p;
+	uint64_t		seed;
+	const uint8_t	*p;
 
-    seed = 0xa0761d6478bd642fULL;
-    p = (const uint8_t *)str;
-    while (*p)
-    {
-        seed ^= (uint64_t)*p++;
-        seed *= 0x9e3779b97f4a7c15ULL;
-    }
-    return (seed ^ (seed >> 32));
+	seed = 0xa0761d6478bd642fULL;
+	p = (const uint8_t *)str;
+	while (*p)
+	{
+		seed ^= (uint64_t) *p++;
+		seed *= 0x9e3779b97f4a7c15ULL;
+	}
+	return (seed ^ (seed >> 32));
 }
+
 /*
  * Extract 7 bit h2 for metadata from ur code
 */
-uint8_t h2(uint64_t hash)
+uint8_t	h2(uint64_t hash)
 {
-    return ((hash >> 57) & 0x7F);
+	return ((hash >> 57) & 0x7F);
 }
--- main.c
+++ main.c
@@ -3,75 +3,75 @@
 // Process input key-value pairs
 static int	handle_pair(t_hotrace *hr, char *line)
 {
-    char	*key;
-    char	*value;
+	char	*key;
+	char	*value;
 
-    key = ft_strdup_arena(hr, line, __builtin_strlen(line));
-    if (!key)
-        return (write_str("Arena Allocation failed for key\n"), 0);
-    line = read_line(hr);
-    if (!line)
-	return (0);
-    value = ft_strdup_arena(hr, line, __builtin_strlen(line));
-    if (!value)
-        return (write_str("Arena Allocation failed for value\n"), 0);
-    if (!insert(hr, key, value))
-    {
-        write_str("Insertion failed for key: ");
-        write_str(key);
-        write_str("\n");
-        return (0);
-    }
-    return (1);
+	key = ft_strdup_arena(hr, line, __builtin_strlen(line));
+	if (!key)
+		return (write_str("Arena Allocation failed for key\n"), 0);
+	line = read_line(hr);
+	if (!line)
+		return (0);
+	value = ft_strdup_arena(hr, line, __builtin_strlen(line));
+	if (!value)
+		return (write_str("Arena Allocation failed for value\n"), 0);
+	if (!insert(hr, key, value))
+	{
+		write_str("Insertion failed for key: ");
+		write_str(key);
+		write_str("\n");
+		return (0);
+	}
+	return (1);
 }
 
-static void	process_input(t_hotrace *hr){
-    char	*line;
+static void	process_input(t_hotrace *hr)
+{
+	char	*line;
 
-    while (1)
-    {
-        line = read_line(hr);
-        if (!line || !line[0])
-	    break;
-        if (!handle_pair(hr, line))
-	    break;
-    }
+	while (1)
+	{
+		line = read_line(hr);
+		if (!line || !line[0])
+			break ;
+		if (!handle_pair(hr, line))
+			break ;
+	}
 }
 
 static void	process_queries(t_hotrace *hr)
 {
-    char	*line;
-    char	*value;
+	char	*line;
+	char	*value;
 
-    while (1) 
-    {
-        line = read_line(hr);
-        if (!line)
-	    break;
-        if (!line[0])
-	    continue;
-        value = search(hr, line);
-        if (value)
-	    write_str(value);
-        else
+	while (1) 
 	{
-            write_str(line);
-            write_str(": Not found.");
-        }
-        write_str("\n");
-    }
+		line = read_line(hr);
+		if (!line)
+			break ;
+		if (!line[0])
+			continue ;
+		value = search(hr, line);
+		if (value)
+			write_str(value);
+		else
+		{
+			write_str(line);
+			write_str(": Not found.");
+		}
+		write_str("\n");
+	}
 }
 
-int main(void)
+int	main(void)
 {
-    t_hotrace	*hr;
+	t_hotrace	*hr;
 
-    hr = init_hotrace();
-    if (!hr)
-	return (1);
-    process_input(hr);
-    process_queries(hr);
-    free_hotrace(hr);
-    while(1) pause();
-    return (0);
+	hr = init_hotrace();
+	if (!hr)
+		return (1);
+	process_input(hr);
+	process_queries(hr);
+	free_hotrace(hr);
+	return (0);
 }
